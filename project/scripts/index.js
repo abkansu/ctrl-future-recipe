86149704cb37538a71248c5e54b1ca6f67ed0b3d
Ahmet Bera kansu <abkansu@gmail.com>
2023-08-07 15:57:45 +0300

    Add files via upload

---
+++ project/scripts/index.js
@@ -0,0 +1,61 @@
+function onClickPotatoSalad(){
+    window.location.href = "./recipelist/potato_salad.html";
+}
+
+function onClickParmesanChicken(){
+    window.location.href = "./recipelist/parmesan_chicken.html";
+}
+
+function onClickSummerSalad(){
+    window.location.href = "./recipelist/summer_salad.html";
+}
+
+
+function onClickFrozenChicken(){
+    window.location.href = "./recipelist/frozen_chicken.html";
+}
+
+
+function onClickSaladStuffed(){
+    window.location.href = "./recipelist/salad_stuffed.html";
+}
+
+function onClickSalmonCakes(){
+    window.location.href = "./recipelist/salmon_cakes.html";
+}
+
+function onClickFruitPops(){
+    window.location.href = "./recipelist/fruit_pops.html";
+}
+
+function onClickGalaxyPops(){
+    window.location.href = "./recipelist/galaxy_pops.html";
+}
+
+function onClickLeftoverPops(){
+    window.location.href = "./recipelist/leftover_pops.html";
+}
+
+function onClickIcePops(){
+    window.location.href = "./recipelist/ice_pops.html";
+}
+
+function onClickTropicalPops(){
+    window.location.href = "./recipelist/tropical_pops.html";
+}
+
+function onClickAbout(){
+    window.location.href = "./about.html";
+}
+
+function onClickAboutOne(){
+    window.location.href = "../about.html";
+}
+
+function onClickChefs(){
+    window.location.href = "./chefs.html";
+}
+
+function onClickChefsOne(){
+    window.location.href = "../chefs.html";
+}
